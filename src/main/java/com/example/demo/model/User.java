07c0b6ef99226f8f9f317a3fb04e0ecc38fcfe92
lihf <lihf@10.160.95.19>
2019-04-03 15:53:08 +0800

    init

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+/**
+ * @author lihf
+ *
+ */
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+	private String password;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+
+}
